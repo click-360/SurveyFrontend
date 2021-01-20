cf491e7ef0021575706f22118ef81b2997aa6148
Click 360 <cpn@click-360.com>
2021-01-20 12:19:28 +0530

    color changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,18 @@
 export default {
+    // BgColor: {
+    //     first:{ color: '#f4eff5', value: 1},
+    //     second:{ color: '#e9ddeb', value: 2},
+    //     third:{ color: '#ceb5cd', value: 3},
+    //     fourth:{ color: '#a56da1', value: 4},
+    //     fifth:{ color: '#852d7b', value: 5},
+    //     sixth:{ color: '#770b6b', value: 6}
+    // }
     BgColor: {
-        first:{ color: '#f4eff5', value: 1},
-        second:{ color: '#e9ddeb', value: 2},
-        third:{ color: '#ceb5cd', value: 3},
-        fourth:{ color: '#a56da1', value: 4},
-        fifth:{ color: '#852d7b', value: 5},
-        sixth:{ color: '#770b6b', value: 6}
+        first:{ color: '#ff0d0d', value: 1},
+        second:{ color: '#ff4e11', value: 2},
+        third:{ color: '#ff8e15', value: 3},
+        fourth:{ color: '#acb334', value: 4},
+        fifth:{ color: '#69b34c', value: 5},
+        sixth:{ color: '#00f701', value: 6}
     }
 }
